fe6202173419758cd35074e71e18675c4c499122
LeaoCarvalho <matheus.eu.pessoal@gmail.com>
2024-11-15 17:19:08 -0300

    refactor: add a ResourceManagementStrategy reference to LivecycleStrategy

--- src/main/java/lifecycle/LivecycleStrategy.java
+++ src/main/java/lifecycle/LivecycleStrategy.java
@@ -1,6 +1,14 @@
 package lifecycle;
 
 public abstract class LivecycleStrategy {
+    protected final ResourceManagementStrategy resources;
+
     public abstract Object getServant();
     public abstract void returnServant(Object servant);
+
+    public LivecycleStrategy(ResourceManagementStrategy resources) {
+        this.resources = resources;
+    }
+
+
 }
