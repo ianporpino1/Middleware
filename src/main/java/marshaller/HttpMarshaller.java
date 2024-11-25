9f7dc4d29202e94da61dfac3c70779e9217c6047
Joaquim Chianca <joaquimchianca@gmail.com>
2024-11-25 12:10:37 -0300

    bugfix: import hashmap

--- src/main/java/marshaller/HttpMarshaller.java
+++ src/main/java/marshaller/HttpMarshaller.java
@@ -6,6 +6,7 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class HttpMarshaller implements IHttpMarshaller{
