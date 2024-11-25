a49417b715e796bed4356155d1e65d6d7fd5938b
Joaquim Chianca <joaquimchianca@gmail.com>
2024-11-25 11:44:59 -0300

    refactor: novos campos ao HTTPMessage

--- src/main/java/message/HTTPMessage.java
+++ src/main/java/message/HTTPMessage.java
@@ -3,9 +3,13 @@
 import org.json.JSONObject;
 
 import java.io.Serializable;
+import java.util.Map;
 
 public record HTTPMessage(
         String httpMethod,
         String resource,
-        JSONObject body
+        JSONObject body,
+        int statusCode,
+        String statusMessage,
+        Map<String, String> headers
 ) implements Serializable {}
